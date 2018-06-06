717996aa807ca293318f7c6a03a45a66b897cda7
callahrs <callahrs@rose-hulman.edu>
2018-06-06 14:17:33 -0400

    Progress Save

--- src/m3_practice_fixing_errors.py
+++ src/m3_practice_fixing_errors.py
@@ -2,8 +2,9 @@
 This module lets you practice correcting SYNTAX (notation) errors.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Riley Callahan.
+"""
+# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # TODO: 2.
@@ -34,42 +35,48 @@ import math
 
 
 def main():
-    """ Calls the other functions in this module to demo them. """
     print_math()
-    tutle_fn()
+    turtle_fn()
 
 
-def print_math:
+def print_math():
     """ Prints some calculated values. """
-    x = cos(pi)
+    x = math.cos(math.pi)
     print(x)
 
-    y = sin(pi)
-    print(The sine of PI is, y)
+    y = math.sin(math.pi)
+    print('The')
+    print('sine')
+    print('of')
+    print('PI is', y)
 
 
-def turtle_fun():
+def turtle_fn():
     """
     Constructs a TurtleWindow,
     constructs a classic SimpleTurtle and asks it to do some things,
     and waits for the user to click anywhere in the window to close it.
     """
+
     window = rg.TurtleWindow()
 
     alan = rg.SimpleTurtle()
-alan.pen = rg.Pen('blue', 30)
+
+    alan.pen = rg.Pen('blue', 30)
     alan.paint_bucket = rg.PaintBucket('yellow')
 
-    alan.back(3 * (47 + 16)
-    al.begin_fill()
-    alan.circle(25)
+    alan.backward(3 * (47 + 16))
+    alan.begin_fill()
+    alan.draw_circle(25)
     alan.end_fill()
 
-    forward(200)
+    alan.forward(200)
 
     window.close_on_mouse_click()
 
-# ----------------------------------------------------------------------
-# Calls  main  to start the ball rolling.
-# ----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
+    # Calls  main  to start the ball rolling.
+    # ----------------------------------------------------------------------
+
+
 main()
